ec21c011f43aae28e06f632c2ff87851e0c8b013
liuzhenjun <442359694@qq.com>
2021-11-08 18:15:13 +0800

    add

--- src/pages/task/index.vue
+++ src/pages/task/index.vue
@@ -1,22 +1,43 @@
 <!--任务界面  -->
 <template>
-  <van-tabs :active="active">
-    <van-tab title="标签 1">内容 1</van-tab>
-    <van-tab title="标签 2">内容 2</van-tab>
-    <van-tab title="标签 3">内容 3</van-tab>
-    <van-tab title="标签 4">内容 4</van-tab>
-  </van-tabs>
+  <view>
+    <van-divider contentPosition="left">业务信息</van-divider>
+    <van-cell-group>
+      <van-field
+        label="项目名称"
+        :value="projectName"
+        placeholder="请输入用户名"
+        @change="onChange"
+      />
+      <my-select></my-select>
+    </van-cell-group>
+  </view>
 </template>
 
 <script>
+import select from "@/components/select/index.vue";
+console.log(select);
 export default {
+  components: {
+    mySelect: select,
+  },
   data() {
     return {
-      active: 1,
+      projectName: "你好",
+      columns: ["杭州", "宁波", "温州", "嘉兴", "湖州"],
     };
   },
   created() {},
   mounted() {},
+  methods: {
+    onChange(data) {
+      this.projectName = data.detail;
+    },
+    onChange1(data) {
+      const { picker, value, index } = data.detail;
+      Toast(`当前值：${value}, 当前索引：${index}`);
+    },
+  },
 };
 </script>
 <style scoped></style>
